61463faa470d7023b094e77ca9b178743be006c4
luchangdie <1403105250@qq.com>
2017-03-09 20:43:07 +0800

    print log

--- app/src/main/java/lcd/bst/helloworld/MainActivity.java
+++ app/src/main/java/lcd/bst/helloworld/MainActivity.java
@@ -2,13 +2,17 @@ package lcd.bst.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello World！");
+        //System.out.println("Hello World！");
+        //打印日志
+        Log.d(TAG,"onCreate");
     }
 }
